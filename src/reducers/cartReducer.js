b85cf18859d8c71f284a70a3a8d89ea134300c90
Hernan <whiterabbitbuilder@gmail.com>
2021-12-24 00:27:44 -0500

    Update discount Handling

--- src/reducers/cartReducer.js
+++ src/reducers/cartReducer.js
@@ -1,3 +1,4 @@
+import { CollectionsBookmarkOutlined } from "@material-ui/icons";
 import {
   ADD_PRODUCT_TO_CART,
   DELETE_PRODUCT_FROM_CART,
@@ -43,28 +44,35 @@ export const cartReducer = (state = initialState, action) => {
         (product) => product.productId === action.payload
       );
       let currentProductFlag = currentProductAdd[0].flag;
+      console.log("CurrentProductAdd", currentProductAdd[0].saleQuantityIncrement);
       switch (currentProductAdd[0].saleQuantityIncrement) {
         case 2:
           if ((currentProductAdd[0].quantity + 1) % 2 === 0) {
             currentProductFlag = +1;
             totalDiscount = currentProductFlag * currentProductAdd[0].discount;
           }
+          return {
+            ...state,
+            products: state.products.map((item) =>
+              item.productId === action.payload
+                ? {
+                    ...item,
+                    quantity: item.quantity + 1,
+                    flag: currentProductFlag,
+                  }
+                : item
+            ),
+            discount: state.discount + totalDiscount,
+            total: RoundFixed2(
+              state.total -
+                totalDiscount +
+                state.products.find((item) => item.productId === action.payload)
+                  .price
+            ),
+          };
           break;
-        case 3: if ((currentProductAdd[0].quantity + 1) % 3 > 1) {
-          currentProductFlag = +1;
-          totalDiscount = currentProductFlag * currentProductAdd[0].discount;
-        }
-          break;
-        default:
-          break;
-      }
-      if (currentProductAdd[0].saleQuantityIncrement > 0) {
-        if ((currentProductAdd[0].quantity + 1) % 2 === 0) {
-          currentProductFlag = +1;
-          totalDiscount = currentProductFlag * currentProductAdd[0].discount;
-        } else if (
-          currentProductAdd[0].quantity + 1 > 1 && currentProductAdd[0].saleQuantityIncrement
-        ) {
+        case 3: 
+        if ((currentProductAdd[0].quantity + 1) % 3 > 1) {
           currentProductFlag = +1;
           totalDiscount = currentProductFlag * currentProductAdd[0].discount;
         }
@@ -75,34 +83,47 @@ export const cartReducer = (state = initialState, action) => {
               ? {
                   ...item,
                   quantity: item.quantity + 1,
-                  flag: currentProductFlag,
                 }
               : item
           ),
-          total: RoundFixed2(
-            state.total -
-              totalDiscount +
-              state.products.find((item) => item.productId === action.payload)
-                .price
-          ),
-        };
-      } else {
-        return {
-          ...state,
-          products: state.products.map((item) =>
-            item.productId === action.payload
-              ? {
-                  ...item,
-                  quantity: item.quantity + 1,
-                }
-              : item
-          ),
+          discount: state.discount + totalDiscount,
           total:
-            state.total +
+            state.total -
+            totalDiscount +
             state.products.find((item) => item.productId === action.payload)
               .price,
         };
+          break;
+        default:
+          return {
+            ...state,
+            products: state.products.map((item) =>
+              item.productId === action.payload
+                ? {
+                    ...item,
+                    quantity: item.quantity + 1,
+                  }
+                : item
+            ),
+            discount: state.discount,
+            total:
+              state.total +
+              state.products.find((item) => item.productId === action.payload)
+                .price,
+          };
+          break;
       }
+      // if (currentProductAdd[0].saleQuantityIncrement > 0) {
+      //   if ((currentProductAdd[0].quantity + 1) % 2 === 0) {
+      //     currentProductFlag = +1;
+      //     totalDiscount = currentProductFlag * currentProductAdd[0].discount;
+      //   } else if (
+      //     currentProductAdd[0].quantity + 1 > 1 && currentProductAdd[0].saleQuantityIncrement
+      //   ) {
+      //     currentProductFlag = +1;
+      //     totalDiscount = currentProductFlag * currentProductAdd[0].discount;
+      //   }
+ 
 
     case SUBSTRACT_PRODUCT_QUANTITY:
       return {
